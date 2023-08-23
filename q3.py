5844722a2644533249c7e7a80951a51fa4bf7287
Selani-Indrapala <66544479+Selani-Indrapala@users.noreply.github.com>
2023-08-23 09:25:43 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -11,26 +11,34 @@ def gammaCorrection(src, gamma):
     return cv.LUT(src, table)
 
 
-img = cv.imread('Q3.jpg',cv.IMREAD_COLOR)
-img = cv.cvtColor(img,cv.COLOR_BGR2Lab)
-gammaImg = gammaCorrection(img, 2.2)
+Or_img = cv.imread('Q3.jpg',cv.IMREAD_COLOR)
+img = cv.cvtColor(Or_img,cv.COLOR_BGR2Lab)
+L,a,b = cv.split(img)
+'''cv.imshow("L",L)
+cv.imshow("a",a)
+cv.imshow("b",b)
+cv.waitKey(0)'''
 
-cv.imshow('Original image', img)
-cv.imshow('Gamma corrected image', gammaImg)
+L_new = gammaCorrection(L, 2.2)
+gammaImg = cv.merge([L_new,a,b])
+fin_img = cv.cvtColor(gammaImg,cv.COLOR_Lab2BGR)
+
+cv.imshow('Original image', Or_img)
+cv.imshow('Gamma corrected image', fin_img)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 figure, axis = plt.subplots(1, 2)
 
-for i, col in enumerate(['b', 'g', 'r']):
+for i in range(3):
     hist = cv.calcHist([img], [i], None, [256], [0, 256])
-    axis[0].plot(hist, color = col)
+    axis[0].plot(hist)
     axis[0].set_title("Original Image")
     plt.xlim([0, 256])
 
-for i, col in enumerate(['b', 'g', 'r']):
+for i in range(3):
     hist = cv.calcHist([gammaImg], [i], None, [256], [0, 256])
-    axis[1].plot(hist, color = col)
+    axis[1].plot(hist)
     axis[1].set_title("Gamma Corrected Image")
     plt.xlim([0, 256])
     
